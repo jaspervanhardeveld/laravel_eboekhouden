9235bb7e79ed1d4185ee9461487f45dc682d1f60
Daan van Berkel <info@daanvanberkel.nl>
2020-01-10 18:43:52 +0100

    Typo

--- config/eboekhouden.php
+++ config/eboekhouden.php
@@ -3,7 +3,7 @@ return [
     'username' => env('EBOEKHOUDEN_USERNAME'),
     'security_code1' => env('EBOEKHOUDEN_SEC_1'),
     'security_code2' => env('EBOEKHOUDEN_SEC_2'),
-    'wsdl_url' => 'https://soap.e-boekhouden.nl/soap.asmx?wsdl',
+    'wsdl' => 'https://soap.e-boekhouden.nl/soap.asmx?wsdl',
     'payment_term' => 14,
     'invoice_template' => 'Factuur sjabloon',
     'email_from_address' => env('EBOEKHOUDEN_EMAIL_FROM_ADDRESS', 'invoices@example.com'),
